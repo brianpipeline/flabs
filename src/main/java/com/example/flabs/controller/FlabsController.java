587793eef861ed64b7f8c8edb50c0dccf62cfcaf
Brian Kurilko <briankurilko@gmail.com>
2023-10-16 00:07:48 -0400

    Version bump

--- src/main/java/com/example/flabs/controller/FlabsController.java
+++ src/main/java/com/example/flabs/controller/FlabsController.java
@@ -11,6 +11,6 @@ public class FlabsController {
     @GetMapping("/hello-world")
     @ResponseBody
     public String helloWorld() {
-        return "something! WORK PLEASE.";
+        return "FINALLY. Got everything working... kinda. The versioning anyway.";
     }
 }
